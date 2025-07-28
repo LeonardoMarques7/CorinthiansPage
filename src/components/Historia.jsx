7119f3a0fb0ea07bcd1987e8c0e5f1729068c85a
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-07-28 15:09:50 -0300

    feat: alterando historia e finalizando a mesma secao

--- src/components/Historia.jsx
+++ src/components/Historia.jsx
@@ -1,6 +1,7 @@
 import fotoS from "../assets/imagens/s.png";
 import fotoC from "../assets/imagens/c.png";
 import fotoP from "../assets/imagens/p.png";
+import fotoLinhaDoTempo from "../assets/imagens/linha-do-tempo.png";
 
 const Historia = () => {
 	const timelineData = [
@@ -565,17 +566,38 @@ const Historia = () => {
 		<div className="container__historia">
 			<div className="">
 				<h1 className="container__title">História</h1>
-				<div className="container__texts">
-					{timelineData.map((item, index) => (
-						<div key={index} className="">
-							<div className="group__text">
-								<h1 className="text__title">
-									{item.year} {item.title}
-								</h1>
-								<p className="">{item.text}</p>
-							</div>
-						</div>
-					))}
+				<div className="container__texts__historia">
+					<div className="container__texts left">
+						{timelineData
+							.filter((_, i) => i % 2 === 0)
+							.map((item, index) => (
+								<div key={index} className="group__text">
+									<h1 className="text__title">
+										{item.year} {item.title}
+									</h1>
+									<p>{item.text}</p>
+								</div>
+							))}
+					</div>
+
+					<span className="image__historia">
+						<span className="separacao">
+							<img src={fotoLinhaDoTempo}></img>
+						</span>
+					</span>
+
+					<div className="container__texts right">
+						{timelineData
+							.filter((_, i) => i % 2 !== 0)
+							.map((item, index) => (
+								<div key={index} className="group__text">
+									<h1 className="text__title">
+										{item.year} {item.title}
+									</h1>
+									<p>{item.text}</p>
+								</div>
+							))}
+					</div>
 				</div>
 			</div>
 		</div>
