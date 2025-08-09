6b0ba98447509ee2f1428607db15ec12e4ac4884
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-08-08 23:43:25 -0300

    feat: botao fixo na pagina

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,103 @@ import Elenco from "./components/Elenco";
 import Noticias from "./components/Noticias";
 import Footer from "./components/Footer";
 import Partidas from "./components/Partidas";
+import { useEffect } from "react";
+import { Toaster, toast } from "sonner";
+import { Button } from "@/components/ui/button"; // botão do shadcn
+import { ArrowBigUp, Bell, ChevronUpIcon, Quote } from "lucide-react";
+import ronaldoImg from "./assets/imagens/foto__ronaldo.png";
+import socratesImg from "./assets/imagens/foto__socrates.png";
+import marcelinhoImg from "./assets/imagens/foto__marcelinho.png";
+import titeImg from "./assets/imagens/foto__tite.png";
+import robertoImg from "./assets/imagens/foto__roberto.png";
+import useIsMobile from "./components/hooks/useIsMoblie";
+
+// ... seus outros imports
+const contatosLendarios = [
+	{
+		nome: "Sócrates",
+		foto: socratesImg,
+		mensagens: [
+			'"Ganhar ou perder, mas sempre com democracia"', // Símbolo da Democracia Corinthiana :contentReference[oaicite:1]{index=1}
+			'"O Corinthians é muito mais do que um clube… é uma força de expressão"', // Frase icônica retratada em homenagens atuais :contentReference[oaicite:2]{index=2}
+		],
+	},
+	{
+		nome: "Marcelinho Carioca",
+		foto: marcelinhoImg,
+		mensagens: [
+			'"Aqui é minha segunda pele"', // Declarou depois de se apaixonar pelo clube :contentReference[oaicite:3]{index=3}
+			'"Quero marcar minha passagem aqui"', // Palavras da sua apresentação no Parque São Jorge :contentReference[oaicite:4]{index=4}
+		],
+	},
+	{
+		nome: "Ronaldo Fenômeno",
+		foto: ronaldoImg,
+		mensagens: [
+			'"Agora eu sou mais um louco no bando"', // Falou ao adotar a Fiel como sua torcida :contentReference[oaicite:5]{index=5}
+			'"A torcida do Corinthians é a melhor do mundo!"', // Declarou em entrevista elogiando os fãs :contentReference[oaicite:6]{index=6}
+		],
+	},
+	{
+		nome: "Tite",
+		foto: titeImg,
+		mensagens: [
+			'"O vencedor é um cara persistente"', // Mantra usado nas preleções em 2011 :contentReference[oaicite:7]{index=7}
+			'"Esse time tem alma de competição"', // Comentou sobre o espírito do elenco aclame :contentReference[oaicite:8]{index=8}
+			'"Ser corinthiano é acreditar que tudo é possível"', // Valoriza fé e confiança no clube :contentReference[oaicite:9]{index=9}
+		],
+	},
+	{
+		nome: "Roberto Rivellino",
+		foto: robertoImg,
+		mensagens: [
+			'"O Corinthians representa tudo na minha vida"', // Disse em fórum “Meu Timão” sobre sua paixão :contentReference[oaicite:10]{index=10}
+			'"Não me considero o melhor, mas meu nome está incluído entre os maiores"', // Autoavaliação com humildade :contentReference[oaicite:11]{index=11}
+		],
+	},
+];
 
 function App() {
+	useEffect(() => {
+		const aoRolar = () => {
+			if (window.scrollY > 400) {
+				setMostrarBotao(true);
+			} else {
+				setMostrarBotao(false);
+			}
+		};
+		window.addEventListener("scroll", aoRolar);
+		return () => window.removeEventListener("scroll", aoRolar);
+	}, []);
+
+	// Subir suavemente até o topo
+	const subirAoTopo = () => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	};
+
+	// Mostrar o botão ao rolar a página
+
+	const isMobile = useIsMobile();
+
+	const chamadaRonaldo = () => {
+		const contato =
+			contatosLendarios[Math.floor(Math.random() * contatosLendarios.length)];
+
+		// Escolhe uma mensagem aleatória desse contato
+		const mensagem =
+			contato.mensagens[Math.floor(Math.random() * contato.mensagens.length)];
+
+		toast.custom(() => (
+			<div className="toast__whatsapp">
+				<img src={contato.foto} alt={contato.nome} />
+				<div className="toast__whatsapp__texts">
+					<span>{contato.nome}</span>
+					<p>{mensagem}</p>
+				</div>
+			</div>
+		));
+	};
+
 	return (
 		<div className="page-content">
 			{/* Barra de Navegação */}
@@ -17,6 +112,12 @@ function App() {
 
 			{/* Tela Principal */}
 			<Main />
+			<Toaster
+				position={isMobile ? "bottom-right" : "top-right"}
+				className="toaster"
+			/>
+
+			{/* Botão fixo */}
 
 			{/* Partidas */}
 			<Partidas />
@@ -44,6 +145,14 @@ function App() {
 
 			{/* Footer */}
 			<Footer />
+			<span className="container__actions">
+				<Button className={`botao-topo`} onClick={subirAoTopo}>
+					<ChevronUpIcon size={20} />
+				</Button>
+				<Button onClick={chamadaRonaldo} className="button__trigger">
+					<Quote />
+				</Button>
+			</span>
 		</div>
 	);
 }
--- src/components/Historia.jsx
+++ src/components/Historia.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 
-import { Toaster, toast } from "sonner";
 import {
 	Accordion,
 	AccordionContent,
@@ -11,60 +10,8 @@ import timelineData from "../assets/timelineData";
 import postagensData from "../assets/postagensData";
 import emblema from "../assets/imagens/emblema__header.png";
 import ScrollReveal from "scrollreveal";
-import { Button } from "@/components/ui/button"; // botão do shadcn
-import { ArrowBigUp, Bell, ChevronUpIcon, Quote } from "lucide-react";
-import ToastRonaldo from "./ui/ToastRonaldo";
-import ronaldoImg from "../assets/imagens/foto__ronaldo.png";
-import socratesImg from "../assets/imagens/foto__socrates.png";
-import marcelinhoImg from "../assets/imagens/foto__marcelinho.png";
-import titeImg from "../assets/imagens/foto__tite.png";
-import robertoImg from "../assets/imagens/foto__roberto.png";
-import useIsMobile from "./hooks/useIsMoblie";
 
 // ... seus outros imports
-const contatosLendarios = [
-	{
-		nome: "Sócrates",
-		foto: socratesImg,
-		mensagens: [
-			'"Ganhar ou perder, mas sempre com democracia"', // Símbolo da Democracia Corinthiana :contentReference[oaicite:1]{index=1}
-			'"O Corinthians é muito mais do que um clube… é uma força de expressão"', // Frase icônica retratada em homenagens atuais :contentReference[oaicite:2]{index=2}
-		],
-	},
-	{
-		nome: "Marcelinho Carioca",
-		foto: marcelinhoImg,
-		mensagens: [
-			'"Aqui é minha segunda pele"', // Declarou depois de se apaixonar pelo clube :contentReference[oaicite:3]{index=3}
-			'"Quero marcar minha passagem aqui"', // Palavras da sua apresentação no Parque São Jorge :contentReference[oaicite:4]{index=4}
-		],
-	},
-	{
-		nome: "Ronaldo Fenômeno",
-		foto: ronaldoImg,
-		mensagens: [
-			'"Agora eu sou mais um louco no bando"', // Falou ao adotar a Fiel como sua torcida :contentReference[oaicite:5]{index=5}
-			'"A torcida do Corinthians é a melhor do mundo!"', // Declarou em entrevista elogiando os fãs :contentReference[oaicite:6]{index=6}
-		],
-	},
-	{
-		nome: "Tite",
-		foto: titeImg,
-		mensagens: [
-			'"O vencedor é um cara persistente"', // Mantra usado nas preleções em 2011 :contentReference[oaicite:7]{index=7}
-			'"Esse time tem alma de competição"', // Comentou sobre o espírito do elenco aclame :contentReference[oaicite:8]{index=8}
-			'"Ser corinthiano é acreditar que tudo é possível"', // Valoriza fé e confiança no clube :contentReference[oaicite:9]{index=9}
-		],
-	},
-	{
-		nome: "Roberto Rivellino",
-		foto: robertoImg,
-		mensagens: [
-			'"O Corinthians representa tudo na minha vida"', // Disse em fórum “Meu Timão” sobre sua paixão :contentReference[oaicite:10]{index=10}
-			'"Não me considero o melhor, mas meu nome está incluído entre os maiores"', // Autoavaliação com humildade :contentReference[oaicite:11]{index=11}
-		],
-	},
-];
 
 const Historia = () => {
 	useEffect(() => {
@@ -86,45 +33,6 @@ const Historia = () => {
 			reset: false,
 		});
 	}, []);
-
-	const chamadaRonaldo = () => {
-		const contato =
-			contatosLendarios[Math.floor(Math.random() * contatosLendarios.length)];
-
-		// Escolhe uma mensagem aleatória desse contato
-		const mensagem =
-			contato.mensagens[Math.floor(Math.random() * contato.mensagens.length)];
-
-		toast.custom(() => (
-			<div className="toast__whatsapp">
-				<img src={contato.foto} alt={contato.nome} />
-				<div className="toast__whatsapp__texts">
-					<span>{contato.nome}</span>
-					<p>{mensagem}</p>
-				</div>
-			</div>
-		));
-	};
-
-	// Mostrar o botão ao rolar a página
-	useEffect(() => {
-		const aoRolar = () => {
-			if (window.scrollY > 400) {
-				setMostrarBotao(true);
-			} else {
-				setMostrarBotao(false);
-			}
-		};
-		window.addEventListener("scroll", aoRolar);
-		return () => window.removeEventListener("scroll", aoRolar);
-	}, []);
-
-	// Subir suavemente até o topo
-	const subirAoTopo = () => {
-		window.scrollTo({ top: 0, behavior: "smooth" });
-	};
-
-	const isMobile = useIsMobile();
 
 	return (
 		<div className="container__historia relative w-full" id="Historia">
@@ -193,18 +101,6 @@ const Historia = () => {
 					</a>
 				</span>
 			</div>
-			<span className="container__actions">
-				<Button className={`botao-topo`} onClick={subirAoTopo}>
-					<ChevronUpIcon size={20} />
-				</Button>
-				<Button onClick={chamadaRonaldo} className="button__trigger">
-					<Quote />
-				</Button>
-			</span>
-			<Toaster
-				position={isMobile ? "bottom-right" : "top-right"}
-				className="toaster"
-			/>
 		</div>
 	);
 };
