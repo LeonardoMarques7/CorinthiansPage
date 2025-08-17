2c01c7952d98b8319d77142524df7b730b0b1eee
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-08-17 19:09:57 -0300

    feat: adicionando loja, carroseul e reponsividade

---
+++ src/assets/produtosData.jsx
@@ -0,0 +1,101 @@
+import camisaAwayTorcedor2024 from "./imagens/produtos/camisa-away-torcedor-2024.png";
+import camisaHomeTorcedor2025 from "./imagens/produtos/camisa-home-torcedor-2025.png";
+import blusaoCorinthiansInfantil from "./imagens/produtos/blusa-infantil.png";
+import jaquetaCorinthians from "./imagens/produtos/jaqueta-masculina.png";
+import camisaAwayFerminina2025 from "./imagens/produtos/camisa-away-feminina-torcedor-2025.png";
+import camisaHomeTorcedorInfantil2025 from "./imagens/produtos/camisa-away-infantil-torcedor-2025.png";
+import calcaMasculina from "./imagens/produtos/calca-masculina.png";
+import camisaHome2024 from "./imagens/produtos/camisa-home-torcedor-2024.png";
+
+const produtosData = [
+	{
+		id: 1,
+		imagem: camisaAwayTorcedor2024,
+		titulo: "Camisa Nike Corinthians II 2024/25 Torcedor Pro Masculina",
+		descricao:
+			"Mostre sua paixão pelo Timão com o modelo Away 2024/25. Conforto e estilo para quem carrega a tradição alvinegra no peito.",
+		preco: "R$ 349,99",
+		precoAntigo: "R$ 349,99",
+		link: "https://www.nike.com.br/camisa-nike-corinthians-ii-2024-25-torcedor-pro-masculina-028729.html",
+		avaliacao: { nota: 4.8, total: 217 },
+	},
+	{
+		id: 2,
+		imagem: camisaHomeTorcedor2025,
+		titulo: "Camisa Nike Corinthians I 2025/26 Torcedor Pro Masculina",
+		descricao:
+			"O manto oficial da Fiel para 2025/26. Tecido leve, escudo em destaque e design inspirado na tradição corinthiana.",
+		preco: "R$ 349,99",
+		precoAntigo: "R$ 349,99",
+		link: "https://www.nike.com.br/camisa-nike-corinthians-i-2025-26-torcedor-pro-masculina-058804.html",
+		avaliacao: { nota: 4.9, total: 410 },
+	},
+	{
+		id: 3,
+		imagem: jaquetaCorinthians,
+		titulo: "Jaqueta Nike Corinthians Hino Academy Pro Masculina",
+		descricao:
+			"Estilo e proteção para os dias frios. A jaqueta oficial do Timão combina conforto premium com o orgulho de ser Corinthians.",
+		preco: "R$ 664,99",
+		precoAntigo: "R$ 699,99",
+		link: "https://www.nike.com.br/jaqueta-nike-corinthians-hino-academy-pro-masculina-031445.htm",
+		avaliacao: { nota: 4.7, total: 152 },
+	},
+	{
+		id: 4,
+		imagem: blusaoCorinthiansInfantil,
+		titulo: "Blusão Nike Corinthians Club Sportswear Infantil",
+		descricao:
+			"Proteção e estilo para os pequenos torcedores. O blusão infantil do Timão combina conforto com o orgulho de ser Corinthians.",
+		preco: "R$ 379,99",
+		precoAntigo: "R$ 399,99",
+		link: "https://www.nike.com.br/blusao-nike-sportswear-corinthians-club-infantil-058990.html",
+		avaliacao: { nota: 4.6, total: 85 },
+	},
+	{
+		id: 5,
+		imagem: camisaAwayFerminina2025,
+		titulo: "Camisa Nike Corinthians II 2025/26 Torcedora Pro Feminina",
+		descricao:
+			"A versão feminina do manto Away 2025/26. Conforto e caimento perfeito para as torcedoras que apoiam o Timão em todas as ocasiões.",
+		preco: "R$ 332,49",
+		precoAntigo: "R$ 349,99",
+		link: "https://www.nike.com.br/camisa-nike-corinthians-ii-2025-26-torcedora-pro-feminina-059005.html",
+		avaliacao: { nota: 4.9, total: 112 },
+	},
+	{
+		id: 6,
+		imagem: camisaHomeTorcedorInfantil2025,
+		titulo: "Camisa Nike Corinthians I 2025/26 Torcedor Pro Infantil",
+		descricao:
+			"O manto do Timão para a garotada. Feita com tecido leve para os pequenos torcedores mostrarem a paixão pelo Corinthians.",
+		preco: "R$ 284,99 ",
+		precoAntigo: "R$ 299,99",
+		link: "https://www.nike.com.br/camisa-nike-corinthians-i-2025-26-torcedor-pro-infantil-058809.htm",
+		avaliacao: { nota: 5.0, total: 78 },
+	},
+	{
+		id: 7,
+		imagem: calcaMasculina,
+		titulo: "Calça Nike Corinthians Dri-FIT Masculina",
+		descricao:
+			"Conforto e liberdade para o seu treino. A calça de treino oficial do Timão é ideal para atividades físicas e para o dia a dia.",
+		preco: "R$ 617,49",
+		precoAntigo: "R$ 649,99",
+		link: "https://www.nike.com.br/calca-nike-corinthians-dri-fit-masculina-031461.html",
+		avaliacao: "",
+	},
+	{
+		id: 8,
+		imagem: camisaHome2024,
+		titulo: "Camisa Nike Corinthians I 2024/25 Performance Masculina",
+		descricao:
+			"Estilo e proteção com o boné oficial do Timão. Feito em algodão, com o escudo do Corinthians bordado.",
+		preco: "R$ 313,49 ",
+		precoAntigo: "R$ 549,99",
+		link: "https://www.nike.com.br/camisa-nike-corinthians-i-2024-25-performance-masculina-029863.html",
+		avaliacao: "",
+	},
+];
+
+export default produtosData;
--- src/components/Loja.jsx
+++ src/components/Loja.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import { Expand, ShoppingCart } from "lucide-react";
 import camisaAwayTorcedor2025 from "../assets/imagens/produtos/camisa-away-torcedor-2025.png";
-import camisaAwayTorcedor2024 from "../assets/imagens/produtos/camisa-away-torcedor-2024.png";
-import camisaHomeTorcedor2025 from "../assets/imagens/produtos/camisa-home-torcedor-2025.png";
-import blusaoCorinthians from "../assets/imagens/produtos/blusao.png";
-import { Expand, Eye, ShoppingCart, View } from "lucide-react";
+import produtosData from "../assets/produtosData";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const Loja = () => {
 	return (
@@ -15,123 +20,131 @@ const Loja = () => {
 					você será redirecionado para o site oficial.
 				</span>
 			</div>
+
 			<div className="loja__content">
 				<div className="loja__item__destaque">
 					<img
 						src={camisaAwayTorcedor2025}
 						className="loja__item__image"
-						alt=""
+						alt="Camisa Away Corinthians 2025"
 					/>
 					<div className="item__destaque__texts">
 						<div className="item__destaque__header">
 							Camisas <div className="separacao__div"></div> Edição Especial
 						</div>
+
 						<div className="item__destaque__content">
 							<h2 className="item__destaque__title">
-								Camisa Oficial Corinthians 2025 - Home
+								Camiseta Away Torcedor Corinthians 2025
 							</h2>
+
+							{/* Avaliação */}
+							<div className="item__destaque__rating flex items-center gap-2">
+								<span className="stars">⭐⭐⭐⭐⭐</span>
+								<span className="rating__number">4.9 (328 avaliações)</span>
+							</div>
+
 							<p className="item__destaque__description">
-								Descubra o produto mais desejado pelos torcedores corinthianos.
-								Uma peça única que representa toda a paixão e tradição do Sport
-								Club Corinthians Paulista. Qualidade premium e design exclusivo
-								para os verdadeiros fiéis.
+								Mostre sua paixão pelo Timão com a{" "}
+								<strong>Camiseta Away Torcedor Corinthians 2025</strong>. Um
+								manto moderno que une tradição, estilo e conforto. Feita para o
+								torcedor que vive o Corinthians dentro e fora dos estádios.
 							</p>
+
+							<ul className="item__destaque__list">
+								<li>✔ Modelo Away 2025 – Torcedor</li>
+								<li>✔ Tecido leve e respirável</li>
+								<li>✔ Escudo oficial em destaque</li>
+								<li>✔ Ideal para jogos e uso casual</li>
+								<li>✔ Design moderno inspirado na Fiel</li>
+							</ul>
 						</div>
+
 						<div className="item__destaque__footer">
+							<button
+								onClick={() =>
+									(window.location.href =
+										"https://www.nike.com.br/camisa-nike-corinthians-ii-2025-26-torcedor-pro-masculina-058825.html")
+								}
+								className="item__destaque__btn"
+							>
+								Comprar Agora
+							</button>
 							<div className="item__destaque__badge">
 								<span className="item__destaque__preco">R$ 299,90</span>
 								<span className="item__destaque__preco__antigo">R$ 349,90</span>
 							</div>
-							<a href="#" className="item__destaque__btn">
-								Adquirir Agora
-							</a>
 						</div>
 					</div>
 				</div>
+
 				<div className="separator__loja">COLEÇÃO COMPLETA</div>
-				<ul className="gap-10 items-center container__produtos">
-					<li className="item__produto">
-						<div className="produto__header__card">
-							<img
-								src={camisaAwayTorcedor2024}
-								className="produto__card__image"
-								alt="Imagem do Produto"
-							/>
-						</div>
-						<div className="produto__content__card">
-							<h2 className="produto__card__title">
-								Camisa Nike Corinthians II 2024/25 Torcedor Pro Masculina
-							</h2>
-							<p className="produto__card__description">
-								Produto oficial licenciado do Sport Club Corinthians Paulista.
-								Qualidade garantida e design exclusivo.
-							</p>
-							<div className="produto__footer__card">
-								<div className="produto__badge">
-									<span className="produto__preco">R$ 199,49</span>
-									<span className="produto__preco__antigo">R$ 349,90</span>
+				<Carousel
+					plugins={[
+						Autoplay({
+							delay: 7000,
+						}),
+					]}
+					className="container__carroseul relative"
+				>
+					<CarouselContent className="carroseul__content gap-4">
+						{produtosData.map((produto) => (
+							<CarouselItem
+								className="item__produto  w-fit z-99 relative basis-[80svw] md:basis-1/4 sm:basis-1/3"
+								key={produto.id}
+							>
+								<div className="produto__header__card">
+									<img
+										src={produto.imagem}
+										className="produto__card__image"
+										alt={produto.titulo}
+									/>
 								</div>
-								<a href="#" className="produto__btn">
-									<Expand /> Ver Produto
-								</a>
-							</div>
-						</div>
-					</li>
-					<li className="item__produto">
-						<div className="produto__header__card">
-							<img
-								src={camisaHomeTorcedor2025}
-								className="produto__card__image"
-								alt="Imagem do Produto"
-							/>
-						</div>
-						<div className="produto__content__card">
-							<h2 className="produto__card__title">
-								Camisa Nike Corinthians I 2025/26 Torcedor Pro Masculina
-							</h2>
-							<p className="produto__card__description">
-								Produto oficial licenciado do Sport Club Corinthians Paulista.
-								Qualidade garantida e design exclusivo.
-							</p>
-							<div className="produto__footer__card">
-								<div className="produto__badge">
-									<span className="produto__preco">R$ 332,49</span>
-									<span className="produto__preco__antigo">R$ 349,90</span>
+								<div className="produto__content__card">
+									<h2 className="produto__card__title">{produto.titulo}</h2>
+
+									{/* Avaliação */}
+									<div className="produto__rating flex items-center gap-2">
+										<span className="stars">⭐⭐⭐⭐⭐</span>
+										<span className="rating__number">
+											{produto.avaliacao.nota ? (
+												<>
+													{produto.avaliacao.nota} ({produto.avaliacao.total}{" "}
+													avaliações)
+												</>
+											) : (
+												<>Sem avaliações</>
+											)}
+										</span>
+									</div>
+
+									<p className="produto__card__description">
+										{produto.descricao}
+									</p>
+
+									<div className="produto__footer__card">
+										<div className="produto__badge">
+											<span className="produto__preco">{produto.preco}</span>
+											<span className="produto__preco__antigo">
+												{produto.precoAntigo}
+											</span>
+										</div>
+										<button
+											onClick={() => (window.location.href = produto.link)}
+											className="produto__btn cursor-pointer"
+										>
+											Ver Produto
+										</button>
+									</div>
 								</div>
-								<a href="#" className="produto__btn">
-									<Expand /> Ver Produto
-								</a>
-							</div>
-						</div>
-					</li>
-					<li className="item__produto">
-						<div className="produto__header__card">
-							<img
-								src={blusaoCorinthians}
-								className="produto__card__image"
-								alt="Imagem do Produto"
-							/>
-						</div>
-						<div className="produto__content__card">
-							<h2 className="produto__card__title">
-								Blusão Nike Corinthians Masculino
-							</h2>
-							<p className="produto__card__description">
-								Produto oficial licenciado do Sport Club Corinthians Paulista.
-								Qualidade garantida e design exclusivo.
-							</p>
-							<div className="produto__footer__card">
-								<div className="produto__badge">
-									<span className="produto__preco">R$ 569,99</span>
-									<span className="produto__preco__antigo">R$ 599,99</span>
-								</div>
-								<a href="#" className="produto__btn">
-									<Expand /> Ver Produto
-								</a>
-							</div>
-						</div>
-					</li>
-				</ul>
+							</CarouselItem>
+						))}
+					</CarouselContent>
+					<div className="carroseul__loja">
+						<CarouselPrevious className="loja__carroseul__btn" />
+						<CarouselNext className="loja__carroseul__btn" />
+					</div>
+				</Carousel>
 			</div>
 		</section>
 	);
