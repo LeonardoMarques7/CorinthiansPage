de74fe2b05eb7276cbe76b4292477de5f16051e4
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-08-18 01:23:18 -0300

    feat: timeline horizontal da historia

--- src/components/Historia.jsx
+++ src/components/Historia.jsx
@@ -1,50 +1,49 @@
-import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
-} from "@/components/ui/accordion";
 import timelineData from "../assets/timelineData";
 import Postagens from "./Postagens";
-
-// ... seus outros imports
 
 const Historia = () => {
 	return (
 		<div className="container__historia relative w-full" id="Historia">
 			<h1 className="container__title">História</h1>
 			<div className="container__texts__historia">
-				<Postagens />
-				<div className="timeline">
+				<ul className="timeline timeline-snap-icon w-auto max-w-auto timeline-horizontal">
 					{timelineData.map((item, index) => (
-						<div
-							key={index}
-							className={`timeline__item ${index % 2 === 0 ? "left" : "right"}`}
-						>
-							<div className="content">
-								<Accordion
-									collapsible
-									defaultValue={index % 2 === 0 ? "item-1" : undefined}
-								>
-									<AccordionItem value="item-1">
-										<AccordionTrigger className="accordion__historia">
-											{item.year} {item.title}
-										</AccordionTrigger>
-										<AccordionContent className="accordion__content">
-											{item.text}
-										</AccordionContent>
-									</AccordionItem>
-								</Accordion>
-							</div>
-						</div>
-					))}
-				</div>
-			</div>
+						<li key={index} className="my-28 relative">
+							{index !== 0 && <hr />}
 
+							{/* Ano + bolinha alinhados no meio */}
+							<div className="timeline-middle flex items-center gap-3 justify-center">
+								{/* Ano */}
+								<time className="text-md absolute font-extrabold bg-white w-15 h-15 rounded-full flex justify-center text-black items-center">
+									{item.year}
+								</time>
+							</div>
+							{/* Conteúdo alternado */}
+							<div
+								className={`flex flex-col gap-3 !p-10 w-[30svw] mt-4 ${
+									index % 2 === 0
+										? "timeline-start justify-center md:text-end items-end"
+										: "timeline-end justify-center items-start"
+								}`}
+							>
+								<div className="text-xl font-black accordion__historia">
+									{item.title}
+								</div>
+
+								<p className="text-lg w-full leading-relaxed !max-w-auto">
+									{item.text}
+								</p>
+							</div>
+
+							<hr />
+						</li>
+					))}
+				</ul>
+			</div>
 			<div className="footer__texts__historia">
 				<hr />
 				<span className="historia__subtitle">
-					Fonte: Site Oficial do Corinthians
+					Fonte: Site Oficial do Corinthians{" "}
 					<a
 						href="https://www.corinthians.com.br/clube/historia"
 						target="_blank"
