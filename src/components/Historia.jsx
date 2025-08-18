f26155162b1b3e3467e630182d3b4fd239d4bf4b
LeonardoMarques7 <leonardoemcs155@gmail.com>
2025-08-18 16:20:23 -0300

    feat: adicionando responsividade e ultimas alteracoes no campo de historia

--- src/components/Historia.jsx
+++ src/components/Historia.jsx
@@ -1,20 +1,19 @@
 import "keen-slider/keen-slider.min.css";
 import { useKeenSlider } from "keen-slider/react";
 import timelineData from "../assets/timelineData";
+import { useEffect } from "react";
 
 const Historia = () => {
-	const [sliderRef] = useKeenSlider({
-		// Configurações do Keen-Slider
-		loop: true, // Desativa o loop infinito
-		mode: "snap", // Permite que a rolagem "grude" nos slides
+	const [sliderRef, slider] = useKeenSlider({
+		loop: true, // Mantém o loop infinito
+		mode: "snap",
 		slides: {
-			perView: 3, // Espaço entre os slides (em pixels)
+			perView: 3,
 		},
 		breakpoints: {
-			// Configurações para telas menores
 			"(max-width: 768px)": {
 				slides: {
-					perView: 1, // Mostra apenas 1 slide em telas menores que 768px
+					perView: 1,
 					spacing: 15,
 				},
 			},
@@ -28,40 +27,39 @@ const Historia = () => {
 			</div>
 			<ul
 				ref={sliderRef}
-				className="keen-slider timeline !items-end !mb-25 !mt-[-40rem] !max-h-auto !justify-start timeline-snap-icon timeline-compact cursor-grab"
+				className="!timeline !items-end keen-slider !max-h-auto !justify-start timeline-snap-icon w-full timeline-compact cursor-grab"
 			>
 				{timelineData.map((item, index) => (
 					<li
 						key={index}
-						className="keen-slider__slide px-6 py-10 relative flex flex-col items-center"
+						className="keen-slider__slide py-10 relative flex flex-col items-center"
 					>
-						{index !== 0 && <hr />}
+						{index !== 0 && (
+							<hr className="!bg-white !border-white !text-white" />
+						)}
 
-						{/* Ano + bolinha alinhados no meio */}
-						<div className="timeline-middle flex items-center !px-10 gap-3 justify-center">
-							{/* Ano */}
-							<time className="text-md absolute font-extrabold bg-white w-15 h-15 rounded-full flex justify-center text-black items-center">
+						<div className="timeline-middle flex items-center !px-0 !left-0 gap-3 w-fulljustify-center">
+							<time className="text-md absolute font-extrabold bg-white w-15 h-15 rounded-[10px] flex justify-center text-black items-center">
 								{item.year}
 							</time>
 						</div>
-						{/* Conteúdo alternado */}
+
 						<div
-							className={`flex flex-col gap-3 !p-10 mt-4 ${
-								index % 2 === 0
-									? "timeline-start justify-center md:text-start items-"
-									: "timeline-end justify-center items-start"
+							className={`flex flex-col gap-3 justify-center !mx-5 items-center !py-10 ${
+								index % 2 === 0 ? "timeline-start " : "timeline-end "
 							}`}
 						>
 							<div className="text-xl font-black accordion__historia">
 								{item.title}
 							</div>
-
-							<p className="text-lg w-full leading-relaxed min-h-[40rem] max-h-[40rem]">
-								{item.text}
+							<p className="text-lg w-full leading-relaxed min-h-[50rem] max-h-[50rem]">
+								<div className="texts__historia !line-clamp-10 overflow-hidden ">
+									{item.text}
+								</div>
 							</p>
 						</div>
 
-						<hr />
+						<hr className="!bg-white !border-white !text-white" />
 					</li>
 				))}
 			</ul>
